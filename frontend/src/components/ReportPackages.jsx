16fbc4e64c05c82f5156ae824f4ad72d4db95d19
Mohammad Almosallam <moh.almusallam4@gmail.com>
2022-12-21 15:21:12 +0300

    feat: user and package search

--- frontend/src/components/ReportPackages.jsx
+++ frontend/src/components/ReportPackages.jsx
@@ -9,103 +9,147 @@ import {
   Flex,
   Td,
   TableContainer,
-  Button,
   PopoverTrigger,
   PopoverContent,
   PopoverHeader,
-  PopoverBody,
-  PopoverFooter,
   PopoverArrow,
   PopoverCloseButton,
-  PopoverAnchor,
   Popover,
+  Input,
 } from "@chakra-ui/react";
 import { IoCloseOutline, IoCreateOutline } from "react-icons/io5";
 import PackageForm from "./PackageForm";
 
 function ReportPackages(props) {
   const data = props.allData;
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <Collapse in={props.isOpen} animateOpacity>
-      <TableContainer
-        borderRadius={"25px"}
-        w={{ md: "1100px", lg: "100%" }}
-        overflowX={"scroll"}
-      >
-        <Table variant="simple" size={"md"}>
-          <Thead>
-            <Tr>
-              <Th></Th>
-              <Th>Name</Th>
-              <Th>Type </Th>
-              <Th>status </Th>
-              <Th>flagStatus </Th>
-              <Th>Sent by </Th>
-              <Th>Received by </Th>
-              <Th isNumeric>width</Th>
-              <Th isNumeric>height</Th>
-              <Th isNumeric>weight</Th>
-              <Th isNumeric>cost</Th>
-              <Th>insurance</Th>
-              <Th isNumeric>created at</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data.length > 0 &&
-              data.map((eachPackage) => {
-                return (
-                  <Tr key={eachPackage._id}>
-                    <Td>
-                      {
-                        <Flex gap={"1rem"}>
-                          <Popover>
-                            <PopoverTrigger>
-                              <button>
-                                <IoCreateOutline />
+    <>
+      <Input
+        type={"text"}
+        placeholder={"Search..."}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <Collapse in={props.isOpen} animateOpacity>
+        <TableContainer
+          borderRadius={"25px"}
+          w={{ md: "1100px", lg: "100%" }}
+          overflowX={"scroll"}
+        >
+          <Table variant="simple" size={"md"}>
+            <Thead>
+              <Tr>
+                <Th></Th>
+                <Th>Name</Th>
+                <Th>Type </Th>
+                <Th>status </Th>
+                <Th>flagStatus </Th>
+                <Th>Sent by </Th>
+                <Th>Received by </Th>
+                <Th isNumeric>width</Th>
+                <Th isNumeric>height</Th>
+                <Th isNumeric>weight</Th>
+                <Th isNumeric>cost</Th>
+                <Th>insurance</Th>
+                <Th isNumeric>created at</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.length > 0 &&
+                data
+                  .filter((eachPackage) => {
+                    if (searchTerm === "") {
+                      return eachPackage;
+                    } else if (
+                      eachPackage.name
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      eachPackage.width.toString().includes(searchTerm) ||
+                      eachPackage.height.toString().includes(searchTerm) ||
+                      eachPackage.weight.toString().includes(searchTerm) ||
+                      eachPackage.cost.toString().includes(searchTerm) ||
+                      eachPackage.createdAt.toString().includes(searchTerm) ||
+                      eachPackage.type
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      eachPackage.recEmail
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      eachPackage.status
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      eachPackage.sendEmail
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      eachPackage.flagStatus
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                    ) {
+                      return eachPackage;
+                    }
+                  })
+                  .map((eachPackage) => {
+                    return (
+                      <Tr key={eachPackage._id}>
+                        <Td>
+                          {
+                            <Flex gap={"1rem"}>
+                              <Popover>
+                                <PopoverTrigger>
+                                  <button>
+                                    <IoCreateOutline />
+                                  </button>
+                                </PopoverTrigger>
+                                <PopoverContent h={"180px"} overflow={"scroll"}>
+                                  <PopoverArrow />
+                                  <PopoverCloseButton />
+                                  <PopoverHeader>Location List!</PopoverHeader>
+                                  {
+                                    <PackageForm
+                                      name="Edit"
+                                      packageId={eachPackage._id}
+                                    />
+                                  }
+                                </PopoverContent>
+                              </Popover>
+                              <button
+                                onClick={(e) => {
+                                  props.deletePackageHandler(
+                                    e,
+                                    eachPackage._id
+                                  );
+                                }}
+                              >
+                                <IoCloseOutline />
                               </button>
-                            </PopoverTrigger>
-                            <PopoverContent h={"180px"} overflow={"scroll"}>
-                              <PopoverArrow />
-                              <PopoverCloseButton />
-                              <PopoverHeader>Location List!</PopoverHeader>
-                              {
-                                <PackageForm
-                                  name="Edit"
-                                  packageId={eachPackage._id}
-                                />
-                              }
-                            </PopoverContent>
-                          </Popover>
-                          <button
-                            onClick={(e) => {
-                              props.deletePackageHandler(e, eachPackage._id);
-                            }}
-                          >
-                            <IoCloseOutline />
-                          </button>
-                        </Flex>
-                      }
-                    </Td>
-                    <Td>{eachPackage.name}</Td>
-                    <Td>{eachPackage.type}</Td>
-                    <Td>{eachPackage.status}</Td>
-                    <Td>{eachPackage.flagStatus}</Td>
-                    <Td>{eachPackage.sendEmail}</Td>
-                    <Td>{eachPackage.recEmail}</Td>
-                    <Td isNumeric>{eachPackage.width}</Td>
-                    <Td isNumeric>{eachPackage.height}</Td>
-                    <Td isNumeric>{eachPackage.weight}</Td>
-                    <Td isNumeric>{eachPackage.cost}</Td>
-                    <Td>{eachPackage.insurance}</Td>
-                    <Td isNumeric>{eachPackage.createdAt.substring(0, 10)}</Td>
-                  </Tr>
-                );
-              })}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </Collapse>
+                            </Flex>
+                          }
+                        </Td>
+                        <Td>{eachPackage.name}</Td>
+                        <Td>{eachPackage.type}</Td>
+                        <Td>{eachPackage.status}</Td>
+                        <Td>{eachPackage.flagStatus}</Td>
+                        <Td>{eachPackage.sendEmail}</Td>
+                        <Td>{eachPackage.recEmail}</Td>
+                        <Td isNumeric>{eachPackage.width}</Td>
+                        <Td isNumeric>{eachPackage.height}</Td>
+                        <Td isNumeric>{eachPackage.weight}</Td>
+                        <Td isNumeric>{eachPackage.cost}</Td>
+                        <Td>{eachPackage.insurance}</Td>
+                        <Td isNumeric>
+                          {eachPackage.createdAt.substring(0, 10)}
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Collapse>
+    </>
   );
 }
 
--- frontend/src/components/ReportUsers.jsx
+++ frontend/src/components/ReportUsers.jsx
@@ -19,88 +19,111 @@ import {
   PopoverCloseButton,
   PopoverAnchor,
   Popover,
+  Input,
 } from "@chakra-ui/react";
 import { IoCloseOutline, IoCreateOutline } from "react-icons/io5";
 import ProfileForm from "./ProfileForm";
 
 function ReportUsers(props) {
   const data = props.allData;
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <Collapse in={props.isOpen} animateOpacity>
-      <TableContainer
-        borderRadius={"25px"}
-        w={{ md: "1100px", lg: "100%" }}
-        overflowX={"scroll"}
-      >
-        <Table variant="simple" size={"md"}>
-          <Thead>
-            <Tr>
-              <Th></Th>
-              <Th>Name</Th>
-              <Th>Email </Th>
-              <Th>Address </Th>
-              <Th>cards </Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data.length > 0 &&
-              data.map((eachUser) => {
-                return (
-                  <Tr key={eachUser._id}>
-                    <Td>
-                      {
-                        <Flex gap={"1rem"}>
-                          <Popover>
-                            <PopoverTrigger>
-                              <button>
-                                <IoCreateOutline />
+    <>
+      <Input
+        type={"text"}
+        placeholder={"Search..."}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <Collapse in={props.isOpen} animateOpacity>
+        <TableContainer
+          borderRadius={"25px"}
+          w={{ md: "1100px", lg: "100%" }}
+          overflowX={"scroll"}
+        >
+          <Table variant="simple" size={"md"}>
+            <Thead>
+              <Tr>
+                <Th></Th>
+                <Th>Name</Th>
+                <Th>Email </Th>
+                <Th>Address </Th>
+                <Th>cards </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.length > 0 &&
+                data
+                  .filter((eachUser) => {
+                    if (searchTerm === "") {
+                      return eachUser;
+                    } else if (
+                      eachUser.name
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                    ) {
+                      return eachUser;
+                    }
+                  })
+                  .map((eachUser) => {
+                    return (
+                      <Tr key={eachUser._id}>
+                        <Td>
+                          {
+                            <Flex gap={"1rem"}>
+                              <Popover>
+                                <PopoverTrigger>
+                                  <button>
+                                    <IoCreateOutline />
+                                  </button>
+                                </PopoverTrigger>
+                                <PopoverContent h={"180px"} overflow={"scroll"}>
+                                  <PopoverArrow />
+                                  <PopoverCloseButton />
+                                  <PopoverHeader>Location List!</PopoverHeader>
+                                  {
+                                    <ProfileForm
+                                      key={eachUser._id}
+                                      edit={"Edit"}
+                                      _id={eachUser._id}
+                                    />
+                                  }
+                                </PopoverContent>
+                              </Popover>
+                              <button
+                                onClick={(e) => {
+                                  props.deleteUserHandler(e, eachUser._id);
+                                }}
+                              >
+                                <IoCloseOutline />
                               </button>
-                            </PopoverTrigger>
-                            <PopoverContent h={"180px"} overflow={"scroll"}>
-                              <PopoverArrow />
-                              <PopoverCloseButton />
-                              <PopoverHeader>Location List!</PopoverHeader>
-                              {
-                                <ProfileForm
-                                  key={eachUser._id}
-                                  edit={"Edit"}
-                                  _id={eachUser._id}
-                                />
-                              }
-                            </PopoverContent>
-                          </Popover>
-                          <button
-                            onClick={(e) => {
-                              props.deleteUserHandler(e, eachUser._id);
-                            }}
-                          >
-                            <IoCloseOutline />
-                          </button>
-                        </Flex>
-                      }
-                    </Td>
-                    <Td>{eachUser.name}</Td>
-                    <Td>{eachUser.email}</Td>
-                    <Td>{eachUser.address}</Td>
-                    <Td>
-                      {eachUser.cards.length === 0 ? (
-                        <Text textDecor={"line-through"}>
-                          No cards registerd
-                        </Text>
-                      ) : (
-                        eachUser.cards.map((eachCard) => {
-                          return eachCard["type"] + ",";
-                        })
-                      )}
-                    </Td>
-                  </Tr>
-                );
-              })}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </Collapse>
+                            </Flex>
+                          }
+                        </Td>
+                        <Td>{eachUser.name}</Td>
+                        <Td>{eachUser.email}</Td>
+                        <Td>{eachUser.address}</Td>
+                        <Td>
+                          {eachUser.cards.length === 0 ? (
+                            <Text textDecor={"line-through"}>
+                              No cards registerd
+                            </Text>
+                          ) : (
+                            eachUser.cards.map((eachCard) => {
+                              return eachCard["type"] + ",";
+                            })
+                          )}
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Collapse>
+    </>
   );
 }
 
